4da3604994e87e7f023f3de28323ec1292dd248a
Isaac Aryee <isaac.aryee@azubiafrica.org>
2024-10-10 12:52:16 +0000

    First commit.

---
+++ church/pages/leftbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span
+        class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.html">Church Management System</a>
+  </div>
+  <div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+      <ul class="nav" id="side-menu">
+        <li>
+          <a href="?"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
+        </li>
+        <li>
+          <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Members<span class="fa arrow"></span></a>
+          <ul class="nav nav-second-level">
+            <li>
+              <a href="member-search.php">Search Member</a>
+            </li>
+            <li>
+              <a href="add-course.php">Add Member</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Tithe<span class="fa arrow"></span></a>
+          <ul class="nav nav-second-level">
+            <li>
+              <a href="add-subject.php">Add Tithe</a>
+            </li>
+            <li>
+              <a href="view-subject.php">View</a>
+            </li>
+          </ul>
+        </li>
+        <!-- <li>
+          <a href="register.php"><i class="fa fa-bar-chart-o fa-fw"></i>Regsiter<span class="fa arrow"></span></a>
+        </li>
+        <li>
+          <a href="view.php"><i class="fa fa-bar-chart-o fa-fw"></i>View Students<span class="fa arrow"></span></a>
+        </li>
+        <li>
+          <a href="session.php"><i class="fa fa-bar-chart-o fa-fw"></i>Session<span class="fa arrow"></span></a>
+        </li> -->
+        <li>
+          <a href="logout.php"><i class="fa fa-bar-chart-o fa-fw"></i>Logout<span class="fa arrow"></span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ church/pages/register-member.php
@@ -0,0 +1,28 @@
+
+
+<?php
+session_start ();
+
+if (! (isset ( $_SESSION ['login'] ))) {
+	
+	header ( 'location:../index.php' );
+}
+include('../config/DbFunction.php');
+	$obj=new DbFunction();
+	$rs=$obj->showCourse();
+	$rs1=$obj->showCountry();
+	$ses=$obj->showSession();
+	$res1=$ses->fetch_object();
+	//$res1->session;
+	if(isset($_POST['submit'])){
+	
+     
+     $obj->register($_POST['course-short'],$_POST['c-full'],$_POST['fname'],$_POST['mname'],$_POST['lname'],
+     	            $_POST['gname'],$_POST['ocp'],$_POST['gender'],$_POST['income'],$_POST['category'],$_POST['ph'],$_POST['nation']
+     	             , $_POST['mobno'],$_POST['email'],$_POST['country'],$_POST['state'],$_POST['city'],$_POST['padd'],
+     	              $_POST['cadd'],$_POST['board1'],$_POST['board2'],$_POST['roll1'],$_POST['roll2'],$_POST['pyear1'],
+     	              $_POST['pyear2'],$_POST['sub1'],$_POST['sub2'],$_POST['marks1'],$_POST['marks2'],$_POST['fmarks1'],
+     	              $_POST['fmarks2'] ,$_POST['session']);
+	
+}
+?>
